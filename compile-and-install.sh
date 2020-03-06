d59e32ff72864d4d42656045b83358b34ab354c2
Pavel Benacek <pavel.benacek@gmail.com>
2020-03-06 19:56:51 +0100

    Compile & Install script : Error detection during the RPM build was reworked.

--- compile-and-install.sh
+++ compile-and-install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
 
 echo "#####################################################"
 echo "Removing everything related to the P4"
@@ -17,6 +16,10 @@ echo "Compiling & installing the P4 ENV package"
 echo "#####################################################"
 
 rpmbuild -bb p4scl.spec --define scl p4lang-p4-1
+if [ $? -ne 0 ];then
+    echo "Error during the RPM build"
+    exit 1
+fi
 
 echo "Installing P4 ENV ..."
 yum localinstall -y ~/rpmbuild/RPMS/x86_64/*
@@ -26,6 +29,10 @@ echo "Compiling & intalling the P4 TOOLS package"
 echo "#####################################################"
 
 rpmbuild -bb p4scl-tool.spec --define scl p4lang-p4devel-1
+if [ $? -ne 0 ];then
+    echo "Error during the RPM build"
+    exit 1
+fi
 
 echo "Installing P4 TOOLS ..."
 yum localinstall -y ~/rpmbuild/RPMS/x86_64/*
@@ -34,3 +41,5 @@ echo "We are done ...."
 echo "You can run following commands:"
 echo "  - scl enable p4lang-p4-1 bash --> to start the environment without tools"
 echo "  - scl enable p4lang-p4devel-1 bash --> to start the environment with p4c, bmv2 and p4pktgen"
+
+exit 0
